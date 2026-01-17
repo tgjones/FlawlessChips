24b92e8b041a0c3481be3e67fcbdba6102f5a393
Tim Jones <tim@timjones.io>
2026-01-17 10:12:38 +0000

    Improve startup sequence

--- src/FlawlessChips/6502/Flawless6502.cs
+++ src/FlawlessChips/6502/Flawless6502.cs
@@ -11,22 +11,21 @@ public sealed partial class Flawless6502 : ChipSimulator
 
     public void Startup()
     {
-        // Recalculate all nodes until the chip stabilizes
-        StabilizeChip();
-
         // Assert RESET and initialize other inputs
         SetLow(res);
-        SetLow(clk0);
+        SetHigh(clk0);
         SetHigh(rdy);
         SetLow(so);
         SetHigh(irq);
         SetHigh(nmi);
 
+        StabilizeChip();
+
         // Run for 8 cycles so that RESET fully takes effect
         for (var i = 0; i < 8; i++)
         {
-            SetHigh(clk0);
             SetLow(clk0);
+            SetHigh(clk0);
         }
 
         // Deassert RESET so the chip can continue running normally
